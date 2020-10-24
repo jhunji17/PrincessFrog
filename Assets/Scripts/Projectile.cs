db4b7191fd317f421fc2a944d95e229a117d3f65
Bryce Suzuki <brycesuzuki@berkeley.edu>
2020-10-24 16:08:47 -0700

    Added Projectile abstract class and Constants file

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour
+{
+    public int damage;
+
+    public Constants.Type type;
+
+    public abstract void Fire(float angle);
+
+    public abstract GameObject Absorb();
+}
