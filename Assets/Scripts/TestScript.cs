6eb6c8329ea5f83fbf78f955e6059549fd217d84
jhunji17 <jhunji17@gmail.com>
2020-10-24 13:38:04 -0700

    initial commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+   //test script to make sure that the git repo is working
+   
+}
