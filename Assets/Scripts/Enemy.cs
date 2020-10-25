e99beb6a0b2331455bdb3cfbc22cf80b7be3fd16
Jerry Li <jerryli@jerrys-mbp.attlocal.net>
2020-10-24 18:12:01 -0700

    added enemy tracking

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    public GameObject target;
+    public Vector2 playerXAxis;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+    void Update()
+    {
+        playerXAxis = new Vector2(target.transform.position.x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, playerXAxis, speed * Time.deltaTime);
+    }
+}
